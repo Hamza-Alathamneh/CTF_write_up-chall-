3ae1935d981a1e40aca421ed5b37f0da21867331
HAMZA SALIM MOHAMMAD AMIN ALATHAMNEH <hamzaalathamneh06@gmail.com>
2026-02-16 11:54:13 +0300

    Update chall.py

--- chall.py
+++ chall.py
@@ -1,6 +1,9 @@
 import struct
+#Extracted bytes from xmmword_7FF6EBA54900 16 bytes in little endian
 part1=bytes.fromhex("0D8FFF415E89A804F9FACC6850A34E95")
+#Extracted from xmmword_7FF6EBA54910 16 bytes also little endian
 part2=bytes.fromhex("78E18A6BA55AAEF317E55D0B65D8D34C")
+#this two lines were hardcoded in v7/v8 from the pseudo
 part3=struct.pack('<Q', 0x2A5993F984037330)
 part4=struct.pack('<I',0x0B8A83F0)
 
@@ -12,7 +15,7 @@ for i in range(0,len(encrypted_buffer),4):
     val=struct.unpack('<I',chunk)[0]
     decrypted_block=val ^ 0xC3D2E1F0
     intermediate_bytes+=struct.pack('<I',decrypted_block)
-
+#rotate right helper
 def ror(val,r_bits,max_bits=8):
     return ((val & (2 ** max_bits - 1)) >> r_bits % max_bits) | \
         (val << (max_bits - (r_bits % max_bits)) & (2 ** max_bits - 1))
